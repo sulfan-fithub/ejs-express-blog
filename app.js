3bc9ebfe5a5dc0984abd461df3776baaa2e7a20d
sulfanaidid <sulfanaidid1@gmail.com>
2023-08-04 23:03:59 +0700

    Add blog post feature

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const _ = require('lodash');
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -19,7 +19,10 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 
-  res.render("home", {homeContent: homeStartingContent })
+  res.render("home", {
+    homeStartingContent : homeStartingContent,
+    posts : posts 
+  })
 })
 
 app.get("/about", (req, res) => {
@@ -32,6 +35,42 @@ app.get("/contact", (req, res) => {
   res.render("contact", {contact: contactContent })
 })
 
+app.get("/compose", (req, res) => {
+  res.render("compose")
+})
+
+// Define a global array to store posts
+const posts = [];
+
+// Route to handle the form submission and store the posts
+app.post("/compose", (req, res) => {
+  const postArtikel = {
+    title: req.body.postTitle,
+    body: req.body.postContent
+  };
+
+  // Push the new post object to the posts array
+  posts.push(postArtikel);
+
+  // Redirect to the home page after storing the post
+  res.redirect("/"); // Assuming the home page URL is "/"
+});
+
+
+app.get("/post/:postName", (req, res) => {
+  const requestedPostName = req.params.postName;
+
+  // Find the post with the given title in the posts array
+  const requestedPost = posts.find(post => _.lowerCase(post.title) === _.lowerCase(requestedPostName));
+
+  if (!requestedPost) {
+    // simple error message
+    return res.send("Post not found");
+  }
+
+  // If the post is found, you can render the "post.ejs" template with the post data
+  res.render("post", { post: requestedPost });
+});
 
 
 
